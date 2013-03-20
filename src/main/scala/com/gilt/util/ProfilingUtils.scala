42a321788238ef8845f834ef5c6d7f2b0f7e70b5
Ugo Matrangolo <umatrangolo@gilt.com>
2013-03-20 14:23:22 +0000

    Avoid log objects creation

--- src/main/scala/com/gilt/util/ProfilingUtils.scala
+++ src/main/scala/com/gilt/util/ProfilingUtils.scala
@@ -2,15 +2,19 @@ package com.gilt.util
 
 import org.slf4j.{LoggerFactory, Logger}
 
+object ProfilingUtils {
+  private[ProfilingUtils] val logger: Logger = LoggerFactory.getLogger(getClass)
+}
+
 trait ProfilingUtils {
-  private val logger: Logger = LoggerFactory.getLogger(getClass)
+  import ProfilingUtils._
 
   def time[R](label: String)(block: => R): R = {
     val t0 = System.currentTimeMillis()
     val result = block    // call-by-name
     val t1 = System.currentTimeMillis()
 
-    logger.info("%s: %sms".format(label, (t1 - t0)))
+    logger.info("%s: %s ms".format(label, (t1 - t0)))
     result
   }
 }
